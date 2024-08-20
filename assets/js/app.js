d2a14f15c8d4a264e7e70928cc78bde79463e83f
JeppeFH <jeppefinch@hotmail.dk>
2024-08-20 09:25:14 +0200

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,61 +1,5 @@
 /*Teaminfo Json opgave*/
 
-let profile = [
-  {
-    profileImg: "https://picsum.photos/300/300?random=1 ",
-    teamName: "Jens Hansen",
-    teamGender: "male",
-    teamUsername: "jhansen1963",
-    teamMail: "jenshansen@gmail.com",
-    teamWebsiteBtn: "Se website",
-  },
-
-  {
-    profileImg: "https://picsum.photos/300/300?random=2 ",
-    teamName: "Julie Hansen",
-    teamGender: "female",
-    teamUsername: "jhansen1963",
-    teamMail: "juliehansen@gmail.com",
-    teamWebsiteBtn: "Se website",
-  },
-
-  {
-    profileImg: "https://picsum.photos/300/300?random=3 ",
-    teamName: "Mogens Hansen",
-    teamGender: "male",
-    teamUsername: "mhansen1963",
-    teamMail: "mogenshansen@gmail.com",
-    teamWebsiteBtn: "Se website",
-  },
-
-  {
-    profileImg: "https://picsum.photos/300/300?random=4 ",
-    teamName: "Karl Hansen",
-    teamGender: "male",
-    teamUsername: "khansen1963",
-    teamMail: "karlhansen@gmail.com",
-    teamWebsiteBtn: "Se website",
-  },
-
-  {
-    profileImg: "https://picsum.photos/300/300?random=5 ",
-    teamName: "Emilie Hansen",
-    teamGender: "female",
-    teamUsername: "ehansen1963",
-    teamMail: "emiliehansen@gmail.com",
-    teamWebsiteBtn: "Se website",
-  },
-
-  {
-    profileImg: "https://picsum.photos/300/300?random=6 ",
-    teamName: "Josefine Hansen",
-    teamGender: "female",
-    teamUsername: "jhansen1963",
-    teamMail: "josefinehansen@gmail.com",
-    teamWebsiteBtn: "Se website",
-  },
-];
-
 const app = {};
 
 app.init = () => {
@@ -74,10 +18,13 @@ app.init = () => {
       </section>
 `;
 
-  profile.forEach((element) => {
-    //team.innerHTML += profileTmpl(element);
-    team.insertAdjacentHTML("beforeend", profileTmpl(element));
-  });
+  fetch("/data/team.json")
+    .then((response) => response.json())
+    .then((teamdata) => {
+      teamdata.forEach((element) => {
+        team.insertAdjacentHTML("beforeend", profileTmpl(element));
+      });
+    });
 };
 
 app.init();
