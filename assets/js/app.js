e627f5b07efb3a092196e07d977b142053c9f839
JeppeFH <jeppefinch@hotmail.dk>
2024-08-20 12:52:25 +0200

    update js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,10 @@
 /*Teaminfo Json opgave*/
 const app = {};
 
-// app.init = () => { /* Udkommenteret. Denne del anvendes kun hvis fetch metode benyttes */
 const team = document.querySelector(".team");
+const teamList = document.querySelector(".team-list");
 
+/* Gittervisning */
 const profileTmpl = (element) =>
   ` <section class="team">
     <div class="team-profile ${element.teamGender}">
@@ -16,45 +17,45 @@ const profileTmpl = (element) =>
       </div>
       </section>
 `;
-// }; /* Udkommenteret. Denne del anvendes kun hvis fetch metode benyttes */
 
-// Fetch metode (sættes ind i bund af app.init() //
-/* fetch("/data/team.json")
-    .then((response) => response.json())
-    .then((teamdata) => {
-      teamdata.forEach((element) => {
-        teamFetch.insertAdjacentHTML("beforeend", profileTmpl(element));
-      });
-    });
-};
-
-app.init(); */
+/* liste visning */
+const profileListTmpl = (element) =>
+  ` <section class="team-list">
+    <div class="team-profile ${element.teamGender}">
+        <div class="team-header">
+        <div class="team-img"><img src="${element.profileImg}" alt=""> </div></div>
+        <div class="team-name"><p> ${element.teamName}</p></div>
+        <div class="team-username"><p> ${element.teamUsername}</p></div>
+        <div class="team-mail"><p> ${element.teamMail}</p></div>
+        <div class="team-website-btn"><a href="#"> ${element.teamWebsiteBtn}</a></div>
+      </div>
+      </section>
+`;
 
 // Async metode //
-/* Nedenstående kode er en nyere - og for de fleste en mere letlæselig 
-måde at fetche .json data. */
 async function fetchPosts() {
   try {
     const response = await fetch("../data/team.json");
     const posts = await response.json();
-
-    posts.forEach((post) => {
-      team.insertAdjacentHTML("beforeend", profileTmpl(post));
-    });
+    return posts;
   } catch (error) {
     console.error("Error fetching or parsing data:", error);
   }
 }
 
-fetchPosts();
+/* Array med brugere */
+let posts = await fetchPosts();
 
-/* Farvesortering af køn */
-let teamImg = document.querySelectorAll(".team-img");
+/* Click event */
+const listIcon = document.querySelector(".fa-bars");
+const gitterIcon = document.querySelector(".fa-grip");
 
-profile.forEach((team, index) => {
-  if (team.teamGender === "male") {
-    teamImg[index].style.border = "solid 4px lightblue";
-  } else if (team.teamGender === "female") {
-    teamImg[index].style.border = "solid 4px pink";
-  }
+listIcon.addEventListener("click", () => {
+  teamList.classList.add("active");
+  team.classList.add("active");
+});
+
+gitterIcon.addEventListener("click", () => {
+  teamList.classList.remove("active");
+  team.classList.remove("active");
 });
